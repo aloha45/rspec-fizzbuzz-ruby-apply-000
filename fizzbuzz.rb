ccd0e3c1ddac594fe9159f6ef3dcc9185d516a2b
Cory Spicer <corys4139@gmail.com>
2020-07-13 22:02:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if (int % 3) && (int % 5)
+  if int % 3 == 0 && int % 5 ==0
     return "FizzBuzz"
   
-  elsif int % 3
+  elsif int % 3 == 0
     return "Fizz"
   
-  else int % 5
+  else int % 5 == 0 
     return "Buzz"
   
   end
